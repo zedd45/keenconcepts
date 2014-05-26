1467a25ddeb167d31acce810e1e9f4e7d9866a00
Chris Keen <christopher.keen@gmail.com>
2014-05-26 13:41:51 -0400

    preserve license info.

--- js/classie.js
+++ js/classie.js
@@ -3,8 +3,8 @@
  * class helper functions
  * from bonzo https://github.com/ded/bonzo
  * MIT license
- * 
- * classie.has( elem, 'my-class' ) -> true/false
+ */ 
+ /* classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
  * classie.toggle( elem, 'my-class' )
